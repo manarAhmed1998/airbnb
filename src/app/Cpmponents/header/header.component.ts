d5cde4ba07f2753dcc2aa9aabeb4ddc0a6ce9ab2
marwa taha <marwa15mahmud11@gmail.com>
2023-07-04 13:36:14 +0300

    mmm

--- src/app/Cpmponents/header/header.component.ts
+++ src/app/Cpmponents/header/header.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { MaterialModule } from 'src/app/shared/material.module';
+import { SignInComponent } from '../sign-in/sign-in.component';
+import { SignUpComponent } from '../sign-up/sign-up.component';
+import { MatDialog ,MatDialogConfig ,MatDialogRef} from '@angular/material/dialog';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<HeaderComponent>) { }
 
+
+  openSignInDialog(): void {
+    const dialogRef = this.dialog.open(SignInComponent, {
+      width: '800px'
+    });
+  }
+
+  openSignUpDialog(): void {
+
+    const dialogRef = this.dialog.open(SignUpComponent,{
+      width:'800px',
+    });
+  }
 }
