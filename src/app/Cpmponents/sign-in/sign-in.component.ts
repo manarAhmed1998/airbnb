b0c39d58a8c2772308bbf245be1d5319dc9b04e2
marwa taha <marwa15mahmud11@gmail.com>
2023-07-04 13:04:15 +0300

    singin/signup

---
+++ src/app/Cpmponents/sign-in/sign-in.component.ts
@@ -0,0 +1,108 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule,FormGroup} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDialogModule ,MatDialogRef,MatDialogConfig,MatDialog} from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
+import { SignUpComponent } from '../sign-up/sign-up.component';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent {
+  constructor(private http: HttpClient, public dialog: MatDialog, public dialogRef: MatDialogRef<SignInComponent>){
+
+}
+
+hide = true;
+
+
+signInForm = new FormGroup({
+  emailControl: new FormControl('', [Validators.required, Validators.email]),
+
+  // usernameControl: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9_]*$'), Validators.minLength(3), Validators.maxLength(20)]),
+
+  passwordControl : new FormControl('', [Validators.required, Validators.maxLength(64),Validators.minLength(8)] )
+
+});
+
+
+getPasswordErrorMessage(){
+  if (this.signInForm.get('passwordControl')?.hasError('required')){
+    return 'Password is required';
+  }
+  if ((this.signInForm.get('passwordControl')?.hasError('minlength')) ||
+   (this.signInForm.get('passwordControl')?.hasError('maxlength'))) {
+    return 'password length [8 ~ 64] ';
+  }
+
+
+  const errorMessage = '';
+  console.log('getPasswordErrorMessage:', errorMessage);
+  return errorMessage; }
+
+ getErrorMessage() {
+  if (this.signInForm.get('emailControl')?.hasError('required')) {
+    return 'You must enter a value';
+  }
+  const errorMessage = this.signInForm.get('emailControl')?.hasError('email') ? 'Not a valid email' : '';
+  console.log('getErrorMessage:', errorMessage);
+  return errorMessage;
+
+  // return  this.signInForm.get('emailControl')?.hasError('email') ? 'Not a valid email' : '';
+
+
+}
+
+// getUsernameErrorMessage() {
+//   if (this.signInForm.get('usernameControl')?.hasError('required')) {
+//     return 'Username is required';
+//   }
+//   if (this.signInForm.get('usernameControl')?.hasError('pattern')) {
+//     return 'Username can only contain alphanumeric characters and underscores';
+//   }
+//   if (this.signInForm.get('usernameControl')?.hasError('minlength')) {
+//     return 'Username must be at least 3 characters long';
+//   }
+//   if (this.signInForm.get('usernameControl')?.hasError('maxlength')) {
+//     return 'Username cannot be more than 20 characters long';
+//   }
+//   return '';
+// }
+
+
+
+submitSignInForm() {
+  const formData = {
+    emailControl: this.signInForm.get('emailControl')?.value,
+    passwordControl: this.signInForm.get('passwordControl')?.value
+  };
+  console.log('submitSignInForm:', formData);
+
+  this.http.post('https://example.com/api/authenticate', formData)
+    .subscribe(
+      (response: any) => {
+        console.log('Sign-in successful!', response);
+        // handle successful authentication (e.g. store the access token or cookie)
+      },
+      (error) => {
+        console.error('Sign-in failed!', error);
+        // display an error message or handle the error in some other way
+      }
+    );
+}
+
+
+openSignUpDialog(): void {
+  this.dialogRef.close();
+  const dialogRef = this.dialog.open(SignUpComponent,{
+    width:'800px'
+
+  });
+}
+  }
+
+
---
+++ src/app/Cpmponents/sign-up/sign-up.component.ts
@@ -0,0 +1,140 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule,FormGroup} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDialogModule ,MatDialogRef,MatDialogConfig} from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+constructor(private http: HttpClient){
+
+}
+  hide = true;
+
+  // emailControl : new FormControl('', [Validators.required, Validators.email]);
+  // phoneCountryControl = new FormControl('', Validators.required);
+  // phoneNumberControl = new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]);
+  // nationalIdControl = new FormControl('', [Validators.required, Validators.minLength(14), Validators.maxLength(14)]);
+  // usernameControl = new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9_]*$'), Validators.minLength(3), Validators.maxLength(20)]);
+
+  signUpForm = new FormGroup({
+    emailControl: new FormControl('', [Validators.required, Validators.email]),
+    phoneCountryControl: new FormControl('', Validators.required),
+    phoneNumberControl: new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
+    nationalIdControl: new FormControl('', [Validators.required, Validators.minLength(14), Validators.maxLength(14)]),
+    usernameControl: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9_]*$'), Validators.minLength(3), Validators.maxLength(20)]),
+    passwordControl : new FormControl('', [Validators.required, Validators.maxLength(64),Validators.minLength(8)] )
+
+  });
+
+
+
+
+  countries = [
+    { name: 'Egypt', code: 'EG', dialCode: '20' },
+    { name: 'Saudi Arabia', code: 'SA', dialCode: '966' },
+    { name: 'United Arab Emirates', code: 'AE', dialCode: '971' },
+
+    { name: 'United States', code: 'US', dialCode: '1' },
+    { name: 'Canada', code: 'CA', dialCode: '1' },
+    { name: 'Mexico', code: 'MX', dialCode: '52' }
+
+
+  ];
+  getPhoneNumberErrorMessage() {
+    if (this.signUpForm.get('phoneNumberControl')?.hasError('required')) {
+      return 'You must enter a phone number';
+    }
+    if (this.signUpForm.get('phoneNumberControl')?.hasError('pattern')) {
+      return 'Your phone number must be 11 digits long';
+    }
+    return '';
+  }
+
+
+  getErrorMessage() {
+    if (this.signUpForm.get('emailControl')?.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return  this.signUpForm.get('emailControl')?.hasError('email') ? 'Not a valid email' : '';
+  }
+
+
+  getNationalIdErrorMessage() {
+    if (this.signUpForm.get('nationalIdControl')?.hasError('required')) {
+      return 'You must enter National ID  number';
+
+  }
+  if ((this.signUpForm.get('nationalIdControl')?.hasError('minLength') )|| (this.signUpForm.get('nationalIdControl')?.hasError('maxlength'))) {
+    return 'Your National ID  number must be 14 digits long';
+  }
+  return '';
+
+
+
+
+  }
+
+ getPasswordErrorMessage(){
+  if (this.signUpForm.get('passwordControl')?.hasError('required')){
+    return 'Password is required';
+  }
+  if ((this.signUpForm.get('passwordControl')?.hasError('minlength')) ||
+   (this.signUpForm.get('passwordControl')?.hasError('maxlength'))) {
+    return 'password length [8 ~ 64] ';
+  }
+
+
+  return '';
+ }
+
+  getUsernameErrorMessage() {
+    if (this.signUpForm.get('usernameControl')?.hasError('required')) {
+      return 'Username is required';
+    }
+    if (this.signUpForm.get('usernameControl')?.hasError('pattern')) {
+      return 'Username can only contain alphanumeric characters and underscores';
+    }
+    if (this.signUpForm.get('usernameControl')?.hasError('minlength')) {
+      return 'Username must be at least 3 characters long';
+    }
+    if (this.signUpForm.get('usernameControl')?.hasError('maxlength')) {
+      return 'Username cannot be more than 20 characters long';
+    }
+    return '';
+  }
+
+
+  submitSignUpForm() {
+    const formData = {
+      emailControl: this.signUpForm.get('emailControl')?.value,
+      phoneCountry: this.signUpForm.get('phoneCountryControl')?.value,
+      phoneNumber: this.signUpForm.get('phoneNumberControl')?.value,
+      nationalId: this.signUpForm.get('nationalIdControl')?.value,
+      username: this.signUpForm.get('usernameControl')?.value
+    };
+
+    this.http.post('https://example.com/api/signup', formData)
+      .subscribe(
+        (result) => {
+          console.log('Sign-up successful!', result);
+          // navigate to a success page or display a success message
+        },
+        (error) => {
+          console.error('Sign-up failed!', error);
+          // display an error message or handle the error in some other way
+        }
+      );
+  }
+
+}
+
+
