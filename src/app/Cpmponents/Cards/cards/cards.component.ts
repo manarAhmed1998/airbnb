55f8f40555ec3f06bb298e241f9efd2468fcb966
manar-byte <67881380+manar-byte@users.noreply.github.com>
2023-07-06 22:22:43 +0300

    prepping all cards

--- src/app/Cpmponents/Cards/cards/cards.component.ts
+++ src/app/Cpmponents/Cards/cards/cards.component.ts
@@ -7,7 +7,17 @@ import { CardsService } from 'src/app/services/cards.service';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
+  cards:any;
   constructor(myService:CardsService){
+    myService.getAllCards().subscribe(
+      {
+        next:(data)=>{//console.log(data)
+          this.cards=data;
+        },
+        error:(err)=>{console.log(err)},
+        complete:()=>{}
+      }
+    )
   }
 
 }
--- src/app/Cpmponents/card-details/card-details.component.ts
+++ src/app/Cpmponents/card-details/card-details.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import{ ActivatedRoute }from '@angular/router';
+import { CardsComponent } from '../Cards/cards/cards.component';
+import { CardsService } from 'src/app/services/cards.service';
 
 @Component({
   selector: 'app-card-details',
@@ -8,8 +10,23 @@ import{ ActivatedRoute }from '@angular/router';
 })
 export class CardDetailsComponent {
   id=0;
-  constructor(myRoute:ActivatedRoute){
+  card:any;
+
+  constructor(myRoute:ActivatedRoute,myService:CardsService){
+
     //specifying the id of each card
     this.id=myRoute.snapshot.params["id"];
+
+    
+    //using the service
+    myService.getCardById(this.id).subscribe(
+      {
+        next:(data)=>{//console.log(data)
+          this.card=data;
+        },
+        error:(err)=>{console.log(err)},
+        complete:()=>{}
+      }
+    )
   }
 }
