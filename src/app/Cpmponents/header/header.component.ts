a7503fb1278af614767425cc6c5a0422398f3240
AmrAbaza29 <134836859+AmrAbaza29@users.noreply.github.com>
2023-07-07 04:21:59 +0300

    AddSignUpAndSignInToBackEnd-SavingTokenInLocalStorage

--- src/app/Cpmponents/header/header.component.ts
+++ src/app/Cpmponents/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from 'src/app/shared/material.module';
 import { SignInComponent } from '../sign-in/sign-in.component';
 import { SignUpComponent } from '../sign-up/sign-up.component';
 import { MatDialog ,MatDialogConfig ,MatDialogRef} from '@angular/material/dialog';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 
 @Component({
@@ -10,8 +11,18 @@ import { MatDialog ,MatDialogConfig ,MatDialogRef} from '@angular/material/dialo
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<HeaderComponent>) { }
+export class HeaderComponent implements OnInit{
+  isLoggedIn=false; 
+  constructor
+   (public dialog: MatDialog, 
+    public dialogRef: MatDialogRef<HeaderComponent>,
+    private authenticationService:AuthenticationService   
+    ) { }
+  ngOnInit(): void {
+      this.authenticationService.isLoggedIn$.subscribe((isLoggedIn)=>{
+      this.isLoggedIn=isLoggedIn;
+    })
+  }
 
 
   openSignInDialog(): void {
