cbb8517fb081e9cdc67ec698b1dbdc7c734b83fb
AmrAbaza29 <134836859+AmrAbaza29@users.noreply.github.com>
2023-07-08 19:17:51 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { MatStepperModule } from '@angular/material/stepper';
+import { AddApartmentComponent } from './add-apartment/add-apartment.component';
 
 let routes:Routes=[
   {path:'add-property' , component: AddPropertyComponent  },
@@ -81,7 +82,8 @@ let routes:Routes=[
     HostReservationComponent,
     RoleDashboardComponent,
     RuleDashboardComponent,
-    AddPropertyComponent
+    AddPropertyComponent,
+    AddApartmentComponent
   ],
   imports: [
     BrowserModule,
